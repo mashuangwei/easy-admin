229e2baee8f0f569a55eff144c1674847ee8e597
masw <masw@lianlian.com>
2019-04-12 19:43:56 +0800

    新增多数据源、mybatisplus

--- easy-system/src/main/java/com/msw/AppRun.java
+++ easy-system/src/main/java/com/msw/AppRun.java
@@ -1,6 +1,8 @@
 package com.msw;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import com.msw.utils.SpringContextHolder;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +13,10 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
  * @author mashuangwei
  * @date 2018/11/15 9:20:19
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @EnableTransactionManagement
 @EnableWebSocketMessageBroker
+@MapperScan({"com.msw.*.*.mapper"})
 public class AppRun {
 
     public static void main(String[] args) {
