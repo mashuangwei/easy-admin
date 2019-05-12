a68e35af709992ef89b4528f18cd8fdd5a34099c
mashuangwei <mashuangwei@aliyun.com>
2019-05-13 01:02:53 +0800

    Update JwtPermissionService.java

--- easy-system/src/main/java/com/msw/modules/security/service/JwtPermissionService.java
+++ easy-system/src/main/java/com/msw/modules/security/service/JwtPermissionService.java
@@ -1,10 +1,9 @@
 package com.msw.modules.security.service;
 
-import com.msw.modules.system.repository.PermissionRepository;
-import com.msw.modules.system.repository.RoleRepository;
-import com.msw.modules.system.domain.Permission;
+
 import com.msw.modules.system.domain.Role;
 import com.msw.modules.system.domain.User;
+import com.msw.modules.system.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
@@ -12,7 +11,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -23,9 +21,11 @@ public class JwtPermissionService {
     @Autowired
     private RoleRepository roleRepository;
 
-    @Autowired
-    private PermissionRepository permissionRepository;
-
+    /**
+     * key的名称如有修改，请同步修改 UserServiceImpl 中的 update 方法
+     * @param user
+     * @return
+     */
     @Cacheable(key = "'loadPermissionByUser:' + #p0.username")
     public Collection<GrantedAuthority> mapToGrantedAuthorities(User user) {
 
@@ -33,11 +33,7 @@ public class JwtPermissionService {
 
         Set<Role> roles = roleRepository.findByUsers_Id(user.getId());
 
-        Set<Permission> permissions = new HashSet<>();
-
-        permissions.addAll(permissionRepository.findByRoles(roles));
-
-        return permissions.stream()
+        return roles.stream().flatMap(role -> role.getPermissions().stream())
                 .map(permission -> new SimpleGrantedAuthority(permission.getName()))
                 .collect(Collectors.toList());
     }
